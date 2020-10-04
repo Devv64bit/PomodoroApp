80c2d2e447bb86b696c24a956394beeee79525a4
Dev <pateldev7v@gmail.com>
2020-10-03 22:36:20 -0400

    minor change

--- pomodoro_app/lib/main.dart
+++ pomodoro_app/lib/main.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pomodoro');     
-}
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
 }
