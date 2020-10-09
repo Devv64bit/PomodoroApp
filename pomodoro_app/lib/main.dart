84371b63c5c3e3847e3bf562511cb76fac94c6cb
Dev <pateldev7v@gmail.com>
2020-10-08 22:11:26 -0400

    added nav bar.

--- pomodoro_app/lib/main.dart
+++ pomodoro_app/lib/main.dart
@@ -22,12 +22,31 @@ class _PomodoroState extends State<Pomodoro> {
   double percent = 0;
   static int timeinMinutes = 25;
   int timeinSec = timeinMinutes * 60;
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Scaffold(
+    return Scaffold(
       backgroundColor: Colors.red,
-      appBar: AppBar(title: Text("POMODORO APP"), backgroundColor: Colors.red),
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text("POMODORO APP"),
+        centerTitle: true,
+        elevation: 0.0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu,
+          color: Colors.black,
+        )
+      ),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+        )
+      ),
+    );
   }
 }
