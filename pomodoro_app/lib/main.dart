e795208f3302a99963b1177d7520f6e6dae1101e
Dev Patel <pateldev7v@gmail.com>
2022-10-27 10:07:44 -0400

    changes

--- pomodoro_app/lib/main.dart
+++ pomodoro_app/lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+// ignore: unused_import
+//import 'package:flutter_sound/flutter_sound.dart';
 import 'package:icofont_flutter/icofont_flutter.dart';
 
 void main() => runApp(MyApp());
@@ -17,11 +18,11 @@ class MyApp extends StatelessWidget {
 }
 
 double percent = 0;
-String time = '25.00'; 
+String time = '25.00';
 int timeinMinutes = 25;
 int timeinSeconds = timeinMinutes * 60;
 var duration = const Duration(seconds: 1);
-var watch  = Stopwatch();
+var watch = Stopwatch();
 
 class Pomodoro extends StatefulWidget {
   @override
@@ -43,9 +44,8 @@ class _PomodoroState extends State<Pomodoro> {
             icon: Icon(
               Icons.menu,
               color: Colors.black,
-            )
-          ),
-        ),
+            )),
+      ),
       body: _buildTomatoTimer(),
     );
   }
@@ -53,83 +53,64 @@ class _PomodoroState extends State<Pomodoro> {
 
 Widget _buildTomatoTimer() {
   return Center(
-    child: Column(
-    children: <Widget>[
-      _showTime(),
-      Expanded(
-        flex: 1,
-        child: Icon(
-          IcoFontIcons.tomato,
-          size: 210.00,
-        ),
+      child: Column(children: <Widget>[
+    _showTime(),
+    Expanded(
+      flex: 1,
+      child: Icon(
+        IcoFontIcons.tomato,
+        size: 210.00,
       ),
-      Row(children: <Widget>[
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: _restart,
-          iconSize: 70,
-        ),
-        IconButton(
-          icon: _isPlaying() ? Icon(Icons.pause) : Icon(Icons.play_arrow),
-          onPressed: _startStopWatch,
-          iconSize: 70,
-        ),
-      ])
-    ]
-  )
-  );
+    ),
+    Row(children: <Widget>[
+      IconButton(
+        icon: Icon(Icons.refresh),
+        onPressed: _restart,
+        iconSize: 70,
+      ),
+      IconButton(
+        icon: _isPlaying() ? Icon(Icons.pause) : Icon(Icons.play_arrow),
+        onPressed: _startStopWatch,
+        iconSize: 70,
+      ),
+    ])
+  ]));
 }
 
-Widget _showTime()
-{
+Widget _showTime() {
   return Center(
     child: Text(
       "$time",
       textAlign: TextAlign.center,
-      style: TextStyle(
-        color: Colors.white,
-        fontSize: 80.0
-      ),
+      style: TextStyle(color: Colors.white, fontSize: 80.0),
     ),
   );
 }
 
-void _startStopWatch()
-{
-  if(_isPlaying())
-  {
+void _startStopWatch() {
+  if (_isPlaying()) {
     watch.stop();
-  } 
-  else
-  {
+  } else {
     watch.start();
     _startTimer();
   }
 }
 
-void _startTimer()
-{
+void _startTimer() {
   Timer(duration, _keepTimerGoing);
 }
 
-bool _isPlaying()
-{
+bool _isPlaying() {
   return watch.isRunning;
 }
 
-void _keepTimerGoing()
-{
-  if(watch.isRunning)
-  {
+void _keepTimerGoing() {
+  if (watch.isRunning) {
     _startTimer();
   }
-
-
 }
 
-void _restart()
-{
+void _restart() {
   watch.stop();
   watch.reset();
-  
 }
